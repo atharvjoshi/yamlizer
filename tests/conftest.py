f47b183b329e6a39f6a6b82fe4211da084a7fb8e
Atharv Joshi <atharvjoshi@ymail.com>
2021-09-21 14:49:47 +0800

    Write fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,22 +48,56 @@ class Info:
         """ """
         self.info = info
 
+
 @pytest.fixture
 def get_data() -> Data:
     """ """
     return Data()
 
+
 @pytest.fixture
-def get_data_child() -> DataChild:
+def get_data_yaml_map() -> dict:
+    """ """
+    return {
+        "data": {
+            "list": [0, 1.0, 2e2, 1 + 2j],
+            "set": {None, True},
+            "tuple": (b"a", "f"),
+        }
+    }
+
+
+@pytest.fixture
+def get_datachild() -> DataChild:
     """ """
     return DataChild()
 
+
 @pytest.fixture
-def get_data_grandchild() -> DataGrandChild:
+def get_datachild_yaml_map() -> dict:
+    """ """
+    return get_data_yaml_map().update({"level": 1})
+
+
+@pytest.fixture
+def get_datagrandchild() -> DataGrandChild:
     """ """
     return DataGrandChild()
+
+
+@pytest.fixture
+def get_datagrandchild_yaml_map() -> dict:
+    """ """
+    return get_datachild_yaml_map().update({"name": "X"})
+
 
 @pytest.fixture
 def get_info() -> Info:
     """ """
     return Info()
+
+
+@pytest.fixture
+def get_info_yaml_map() -> Info:
+    """ """
+    return {"info": get_data_yaml_map()}
